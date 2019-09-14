43f7d33c2970cffd449f67eca3b6fe8e688cef87
= <dominikkossinski@gmail.com>
2019-09-14 14:09:57 +0200

    get colors

--- HelloWorls.py
+++ HelloWorls.py
@@ -1,2 +1,19 @@
+import os
+
+from PIL import Image
+
+
+def get_key(val):
+    return val[0]
+
+
 if __name__ == '__main__':
-    print("Hello World")
+    files = os.listdir("images")
+
+    im = Image.open("images\\" + files[0])
+    im = im.convert('RGB')
+
+    colors = im.getcolors(maxcolors=1000000)
+    colors.sort(key=get_key, reverse=True)
+    for c in colors:
+        print(c)
