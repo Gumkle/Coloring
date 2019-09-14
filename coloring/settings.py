5ab1be725e3d6f99a52592bbf2c1c295ed9e4fe0
Dawid Dziedzic <dziedzicdev@gmail.com>
2019-09-14 13:54:09 +0200

    models setup

--- coloring/settings.py
+++ coloring/settings.py
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'colormatch.apps.ColormatchConfig'
 ]
 
 MIDDLEWARE = [
@@ -76,7 +77,7 @@ WSGI_APPLICATION = 'coloring.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'NAME': os.path.join(BASE_DIR, 'db.mysql'),
     }
 }
 
--- colormatch/models.py
+++ colormatch/models.py
@@ -1,3 +1,12 @@
 from django.db import models
 
-# Create your models here.
+
+class Color(models.Model):
+    name = models.CharField(max_length=72)
+    hex = models.CharField(max_length=7)
+
+
+class Image(models.Model):
+    title = models.CharField(max_length=120)
+    filename = models.CharField(max_length=120)
+    colors = models.ManyToManyField(Color)
